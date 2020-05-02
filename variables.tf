51ce17421cddb590ce6f9c75680eb37c9215b421
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-05-02 23:15:30 +0800

    Add character limit of 14 to name_suffix

--- variables.tf
+++ variables.tf
@@ -5,6 +5,10 @@
 variable "name_suffix" {
   description = "An arbitrary suffix that will be added to the end of the resource name(s). For example: an environment name, a business-case name, a numeric id, etc."
   type        = string
+  validation {
+    condition     = length(var.name_suffix) <= 14
+    error_message = "A max of 14 character(s) are allowed."
+  }
 }
 
 variable "name" {

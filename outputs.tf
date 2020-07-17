af1a442e97442dc7fb7e7bf7fff2107714dce6f9
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-07-18 05:36:41 +0800

    BugFix: vm_instance.network_interface.0.access_config is empty if static_ip is not provided

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ip_address" {
-  description = "The IP address (static if provided, or ephemeral if auto-generated) of the VM instance."
-  value       = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+  description = "The static IP address attached to the VM instance."
+  value       = local.static_ip
 }

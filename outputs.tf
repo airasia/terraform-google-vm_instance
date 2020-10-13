01d3271b4ff3a44d98554febdd3c66036d0b3b6c
Rohan Singh <67792436+rohan-searce@users.noreply.github.com>
2020-10-13 14:17:05 +0530

    added usage_IAM_roles for virtual machine
    added usage_IAM_roles for virtual machine

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
+output "usage_IAM_roles" {
+  description = "Basic IAM role(s) that are generally necessary for using the resources in this module. See https://cloud.google.com/iam/docs/understanding-roles."
+  value = [
+    "roles/compute.instanceAdmin.v1",
+  ]
+}
+
 output "static_ip" {
   description = "The static IP address attached to the VM instance."
   value       = local.static_ip

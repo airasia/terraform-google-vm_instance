31f186728a7570642c89137fb3bcf27666fbf08d
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-10-22 19:37:43 +0800

    New sa_roles output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,14 @@ output "sa_email" {
   description = "Email address of the ServiceAccount that is attached to the VM instance."
   value       = local.vm_sa_email
 }
+
+output "sa_roles" {
+  description = "All roles (except sensitive roles filtered by the module) that are attached to the ServiceAccount generated for this VM."
+  value = (
+    local.create_new_sa == true
+    ?
+    module.service_account.0.roles
+    :
+    ["No new ServiceAccount was generated for this VM. See roles attached to the ServiceAccount '${local.vm_sa_email}' instead."]
+  )
+}
